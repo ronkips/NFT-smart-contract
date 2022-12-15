68a203bf3083e5fc930f6e32841160eb3fc2f7c2
ronkips <kipshillash@gmail.com>
2022-12-15 14:48:10 +0300

    updated the smart contract

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -5,7 +5,13 @@ pragma solidity ^0.8.17; //version of solidity we want our compiler to use
 import "hardhat/console.sol";
  
 contract MyNFT {
- constructor() {
-   console.log("THIS IS MY NFT contract. Welcome.");
- }
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
 }
